57a2666593fc3d70ace6e8688d23923380f0694a
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-11-04 14:37:08 +0530

    initial commit

---
+++ src/main/java/com/carecompass/CareCompassApplication.java
@@ -0,0 +1,11 @@
+package com.carecompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareCompassApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CareCompassApplication.class, args);
+	}
+}
---
+++ src/test/java/com/carecompass/CareCompassTests.java
@@ -0,0 +1,11 @@
+package com.carecompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareCompassTests {
+	@Test
+	void contextLoads() {
+	}
+}
